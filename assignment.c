17c8edc53d4b22cc485b472d1b3553107bf788d5
AlexandraIanE <152512116+AlexandraIanE@users.noreply.github.com>
2024-12-01 23:02:21 +0100

    Update assignment.c

--- assignment.c
+++ assignment.c
@@ -12,7 +12,54 @@ int main(int argc, char *argv[]) {
     int maxrand = 100;
 
     // WRITE YOUR CODE HERE
+    //check to see if number of arguments is correct
+    if (argc != 3) {
+        printf("Incorrect usage. You provided %d arguments. The correct number of arguments is 2", argc);
+        return 1;
+    }
+
+    //checks to see if aruments are integers
+    int num1 = atoi(argv[1]);
+    int num2 = atoi(argv[2]);
+    //checks to see if argments are of correct value if not error
+    if (num1 == 0  || num1 <= 0 || num2 == 0 || num2 <= 0){ 
+        printf("Incorrect usage. The parameters you provided are not positive integers");
+        return 1;
+    }
+
+    //initialize matrix
+    int **matrix = (int **)malloc(num1 * sizeof(int *));
     
+    //allocate place for matrix
+    for (int i = 0; i < num1; i++) {
+        matrix[i] = (int *)malloc(num2 * sizeof(int));
+        for (int j = 0; j < num2; j++){
+            matrix[i][j] = rand() % maxrand + minrand;
+        }
+    }
+
+    //open file to write 
+    FILE *fp = fopen("matrix.txt", "w");
+    if (fp ==NULL) {
+        printf ("Error opening file");
+        return 1;
+    }
+
+    //write matrix in file
+    for (int i = 0; i < num1; i++) {
+        for (int j = 0; j < num2; j++) {
+            fprintf(fp, "%d", matrix[i][j]);
+        }
+        if (i != num1 -1) {
+            fprintf(fp, "\n");
+        }
+    }
+
+    //free allocated space for matrix
+    for (int i = 0 ; i < num1; i++) {
+        free(matrix[i]);
+    }
+    free(matrix);
 
     return 0;
 }
